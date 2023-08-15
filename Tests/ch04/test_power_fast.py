453d73dc56af85a9a2634d11380176f2c36f8ba2
Kshitiz Basnet <85289617+KshitizBasnet2021@users.noreply.github.com>
2023-08-14 22:55:51 -0500

    ch04 test_power_fast coverage and test addition

--- Tests/ch04/test_power_fast.py
+++ Tests/ch04/test_power_fast.py
@@ -3,42 +3,23 @@ import unittest
 from ch04.power_fast import power
 
 
-class TestPower(unittest.TestCase):
+class TestPowerFunction(unittest.TestCase):
 
-    def test_power_of_zero(self):
-        x = 5
-        n = 0
-        self.assertEqual(power(x, n), 1)
+    def test_power_zero_exponent(self):
+        self.assertEqual(power(2, 0), 1)
+        self.assertEqual(power(0, 0), 1)
+        self.assertEqual(power(-2, 0), 1)
 
-    def test_power_of_one(self):
-        x = 10
-        n = 1
-        self.assertEqual(power(x, n), 10)
+    def test_power_positive_exponent(self):
+        self.assertEqual(power(2, 3), 8)
+        self.assertEqual(power(3, 4), 81)
+        self.assertEqual(power(-2, 4), 16)
 
-    def test_positive_power(self):
-        x = 2
-        n = 5
-        self.assertEqual(power(x, n), 32)
 
-    def test_negative_power(self):
-        x = 3
-        n = -3
-        self.assertEqual(power(x, n), 1 / 27)
+    def test_power_large_exponent(self):
+        self.assertEqual(power(2, 10), 1024)
+        self.assertEqual(power(10, 5), 100000)
 
-    def test_large_positive_power(self):
-        x = 2
-        n = 10
-        self.assertEqual(power(x, n), 1024)
-
-    def test_large_negative_power(self):
-        x = 4
-        n = -5
-        self.assertEqual(power(x, n), 1 / 1024)
-
-    def test_large_odd_power(self):
-        x = 3
-        n = 7
-        self.assertEqual(power(x, n), 2187)
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_power_base_zero(self):
+        self.assertEqual(power(0, 5), 0)
+        self.assertEqual(power(0, 0), 1)
