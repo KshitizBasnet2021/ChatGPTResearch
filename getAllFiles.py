01763d0beb5343b09be61fb09cfd2eede4421114
Kshitiz Basnet <85289617+KshitizBasnet2021@users.noreply.github.com>
2023-09-01 14:25:10 -0500

    wrote script to communicate with chat gpt

---
+++ getAllFiles.py
@@ -0,0 +1,54 @@
+import os
+
+
+def getPythonFiles():
+    chapters = ['Ch03', 'Ch04', 'Ch05', 'Ch10', 'Ch12', 'Ch13']
+
+    # Initialize an empty list to store file locations
+    file_locations = []
+    for chapter in chapters:
+
+    # Specify the path to the "Commented_Code" directory
+        commented_code_path = f'Tests_Final/{chapter}/Mutation/Commented_Code'
+
+    # Check if the specified path exists
+        if os.path.exists(commented_code_path):
+            # Iterate through subdirectories and files in the directory
+            for root, _, files in os.walk(commented_code_path):
+                for file in files:
+                    # Get the full path of the file
+                    file_path = os.path.join(root, file)
+                    #if file is a python file
+                    if(file_path.endswith(".py")):
+                        # Append the file location to the list
+                        file_locations.append(file_path)
+
+        else:
+            print(f"The directory '{commented_code_path}' does not exist or is not a directory.")
+
+    # Print the list of file locations
+    # for location in file_locations:
+    #  print(location)
+
+    # print(len(file_locations))
+    return file_locations
+
+# Function to count words in a file
+def count_words_in_file(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+        words = content.split()
+        return len(words)
+
+# List of file locations
+file_locations = getPythonFiles()
+# Initialize a variable to store the total word count
+total_word_count = 0
+
+# Loop through each file location in the list
+for file_location in file_locations:
+    word_count = count_words_in_file(file_location)
+    #print(f"{file_location}: {word_count} words")
+    total_word_count += word_count
+
+#print(f"Total word count across all files: {total_word_count} words")
---
+++ gptmain.py
@@ -0,0 +1,53 @@
+import time
+import openai
+import os
+
+from getAllFiles import getPythonFiles
+
+# Your OpenAI API key
+api_key = "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"
+
+# Function to check if a Python code file is buggy
+def is_code_buggy(file_path):
+    # Implement your logic to check if the code is buggy here
+    # You can use static analysis tools or test the code, for example
+    # For simplicity, let's assume all code is buggy
+    return True
+
+# Function to interact with ChatGPT and correct code if it's buggy
+def chat_with_gpt(code, filename, count):
+    openai.api_key = api_key
+    prompt = (
+        f"Do you see any bugs in the following code, if so, can you fix it:\n\n"
+        f"{code}\n\n"
+    )
+
+    # Make the API request
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+    )
+    time.sleep(5)
+    # Extract and return the generated text
+    generated_text = response.choices[0].text.strip()
+    print(generated_text)
+    return "#"+str(count)+" "+filename+'\n'+ generated_text
+
+# List of Python file locations
+python_files = getPythonFiles()
+
+# Create a text file to store the responses
+output_file = "responses.txt"
+
+# Iterate over the Python files
+with open(output_file, "w") as output_file:
+    count = 0
+    for file_path in python_files:
+        with open(file_path, "r") as python_file:
+            code = python_file.read()
+            count = count + 1
+            corrected_code = chat_with_gpt(code, file_path, count)
+            output_file.write(corrected_code + "\n\n")
+
+print(f"Responses saved in {output_file}")
