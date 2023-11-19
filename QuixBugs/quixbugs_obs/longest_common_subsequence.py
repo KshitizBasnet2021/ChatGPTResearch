9a33b8071419b220599a70ed52979bde776b23ae
Kshitiz Basnet <85289617+KshitizBasnet2021@users.noreply.github.com>
2023-11-18 21:58:55 -0600

    quixbugs screenshot

--- QuixBugs/quixbugs_obs/longest_common_subsequence.py
+++ QuixBugs/quixbugs_obs/longest_common_subsequence.py
@@ -2,7 +2,7 @@ def lIIIllIII(lllIIIlII, IIIlIIIIlIIllIlll):
     if not lllIIIlII or not IIIlIIIIlIIllIlll:
         return ''
     elif lllIIIlII[0] == IIIlIIIIlIIllIlll[0]:
-        return lllIIIlII[0] + lIIIllIII(lllIIIlII[1:], IIIlIIIIlIIllIlll)
+        return lllIIIlII[0] + lIIIllIII(lllIIIlII[1:], IIIlIIIIlIIllIlll[1:])
     else:
         return max(
             lIIIllIII(lllIIIlII, IIIlIIIIlIIllIlll[1:]),
