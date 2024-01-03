167a5f5e4343ccc60fb4d7211fd9d943cd15bf08
Kshitiz Basnet <85289617+KshitizBasnet2021@users.noreply.github.com>
2024-01-03 13:54:56 -0600

    ss

--- QuixBugs/correct_python_programs_without_tests/longest_common_subsequence.py
+++ QuixBugs/correct_python_programs_without_tests/longest_common_subsequence.py
@@ -2,7 +2,7 @@ def longest_common_subsequence(a, b):
     if not a or not b:
         return ''
     elif a[0] == b[0]:
-        return a[0] + longest_common_subsequence(a[1:], b[1:])
+        return a[0] + longest_common_subsequence(a[1:], b)
     else:
         return max(
             longest_common_subsequence(a, b[1:]),
